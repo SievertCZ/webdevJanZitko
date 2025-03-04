15032959a569ff605cad5a93f3440ab886dd20ef
SIEVERT\janzi <jan.zitko@easyfm.cz>
2025-03-04 20:59:56 +0100

    Pokus o interni tailwind

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <title>Osobní peněženka</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
 <!-- ODSAZENI NA OSE X PRO CELY WEB -->
